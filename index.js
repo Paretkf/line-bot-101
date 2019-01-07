fc9bfae52681a78f9115ef396c6aeb432f22b89b
Paretkf <paretk7@gmail.com>
2019-01-08 01:58:18 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const axios = require('axios')
+const request = require('request')
 const app = express()
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
