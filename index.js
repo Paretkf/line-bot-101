128298e3da206aedc8fc62ac0f9874da48184999
Paretkf <paretk7@gmail.com>
2019-01-08 00:33:23 +0700

    set up project

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+
 const express = require('express')
+const bodyParser = require('body-parser')
+const CORS = require('cors')({origin: true})
 const app = express()
-const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
+const port = 7777
+
+app.use(CORS)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('', (req, res) => res.send({
+  current_date: new Date()
+}))
 app.listen(port)
+console.log('Server is running on port: ' + port)
+
+module.exports = app
