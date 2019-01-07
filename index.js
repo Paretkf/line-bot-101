c9e4d9e4185521f340d9122f8e6ededb78a98534
Paretkf <paretk7@gmail.com>
2019-01-08 02:05:25 +0700

    test

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ app.get('/', async (req, res) => {
 })
 app.post('/webhook', (req, res) => {
   let reply_token = req.body.events[0].replyToken
+  console.log(reply_token)
   reply(reply_token)
   res.sendStatus(200)
 })
 function reply(reply_token) {
   let headers = {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer {PAFvHNSUVjxIBYqFKku6anaUSHcKIM1QWIYbFIrVA4R0mgY+zrygvbfWjmSIbNGeldODq4IKNQf/MQ4vIQtKm2eBxoAfRJDsGZxHIMXLgyE38bYqVhgUKctU/KXXdqThKHzBjXmhwFNi2e1CzFoBvAdB04t89/1O/w1cDnyilFU=}'
+      'Authorization': 'Bearer PAFvHNSUVjxIBYqFKku6anaUSHcKIM1QWIYbFIrVA4R0mgY+zrygvbfWjmSIbNGeldODq4IKNQf/MQ4vIQtKm2eBxoAfRJDsGZxHIMXLgyE38bYqVhgUKctU/KXXdqThKHzBjXmhwFNi2e1CzFoBvAdB04t89/1O/w1cDnyilFU='
   }
   let body = JSON.stringify({
       replyToken: reply_token,
