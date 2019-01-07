1e71d7c3ec85cc0b0a3d340086ebb3896f0cb261
Paretkf <paretk7@gmail.com>
2019-01-08 01:49:51 +0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.get('/', async (req, res) => {
   res.json({
     status: 200,
@@ -10,4 +13,26 @@ app.get('/', async (req, res) => {
   })
 })
 
+app.post('/webhook', (req, res) => {
+  let reply_token = req.body.events[0].replyToken
+  reply(reply_token)
+  res.sendStatus(200)
+})
+
+const reply = (token) => {
+  axios.post('https://api.line.me/v2/bot/message/reply', {
+    replyToken: token,
+      messages: [{
+        type: 'text',
+        text: 'Hello'
+      },
+      {
+        type: 'text',
+        text: 'How are you?'
+    }]
+  },
+  {
+    headers: {'Authorization': 'Bearer PAFvHNSUVjxIBYqFKku6anaUSHcKIM1QWIYbFIrVA4R0mgY+zrygvbfWjmSIbNGeldODq4IKNQf/MQ4vIQtKm2eBxoAfRJDsGZxHIMXLgyE38bYqVhgUKctU/KXXdqThKHzBjXmhwFNi2e1CzFoBvAdB04t89/1O/w1cDnyilFU='}
+  })
+}
 app.listen(PORT, () => console.log('application is listening on:', PORT))
